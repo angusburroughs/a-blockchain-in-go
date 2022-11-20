20e9b30792a4bf05513e1e587acaa929491b9168
Angus Burroughs <angusb@saltpay.co>
2022-11-20 13:38:31 +0000

    pow implemented

--- main.go
+++ main.go
@@ -1,66 +1,28 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+	"strconv"
+
+	"github.com/angusburroughs/learnBlockChain/blockchain"
 )
-
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-type BlockChain struct {
-	blocks []*Block
-}
-
-func (b *Block) DeriveHash() {
-	// This will join our previous block's relevant info with the new blocks
-	// [][]byte{b.Data, b.PrevHash} is a 2d array literal, resulting in [0,1][b.Data, b.PrevHash]
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-
-	// This performs the actual hasing algorithm
-	hash := sha256.Sum256(info)
-
-	// this doesn't make sense, look up slice defaults
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-
-	block.DeriveHash()
-	return block
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}
 
 func main() {
 
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("first block after genesis")
 	chain.AddBlock("second block after genesis")
 	chain.AddBlock("third block after genesis")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
 		fmt.Printf("Previous hash: %x\n", block.PrevHash)
 		fmt.Printf("data: %s\n", block.Data)
 		fmt.Printf("hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProofOfWork(block)
+		fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 
 }
